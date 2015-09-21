ac6d9bb5225523a98d928f4bc331be8f214c6743
Kyle Pratuch & Samantha Maturen <student@epicodus.com>
2015-09-21 11:01:44 -0700

    Add restaurants controller

---
+++ README.md
@@ -0,0 +1,22 @@
+Best Restaurants in Town
+==========
+
+By Samantha Maturen and Kyle Pratuch, 21-Sep-2015
+
+Displays a list of restaurants and their properties, and allows you to filter the restaurants by type
+
+Setup
+----------
+* Clone repository:
+```console
+$ git clone https://github.com/samchalle/best-restaurants-angular.git
+```
+* Navigate your browser to student-roster/index.html
+
+Technologies Used
+----------
+JavaScript, AngularJS, HTML, CSS, Bootstrap
+
+License
+----------
+MIT License, Copyright (c) 2015 Samantha Maturen and Kyle Pratuch
---
+++ js/BestRestaurantsController.js
@@ -0,0 +1,21 @@
+bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
+  $scope.restaurants = [
+    { name: "Little Big Burger", type: "Burgers", price: "$" },
+    { name: "Lardo", type: "Sandwiches", price: "$$" }
+  ];
+  
+  $scope.addRestaurant = function() {
+    $scope.restaurants.push({ name: $scope.restaurantName,
+      type: $scope.restaurantType,
+      price: $scope.restaurantPrice });
+
+      $scope.restaurantName = null;
+      $scope.restaurantType = null;
+      $scope.restaurantPrice = null;
+  }
+
+  $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.restaurants.indexOf(restaurant);
+    $scope.students.splice(index, 1);
+  };
+});
