6792fb685cea0845ac89b5c03198316e49c76586
Kyle Pratuch & Samantha Maturen <student@epicodus.com>
2015-09-21 14:45:08 -0700

    Change restaurant type into array of types

--- js/BestRestaurantsController.js
+++ js/BestRestaurantsController.js
@@ -1,12 +1,12 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [
-    { name: "Little Big Burger", type: "Burgers", price: "$" },
-    { name: "Lardo", type: "Sandwiches", price: "$$" }
+    { name: "Little Big Burger", type: ["Burgers", "American"], price: "$" },
+    { name: "Lardo", type: ["Sandwiches"], price: "$$" }
   ];
 
   $scope.addRestaurant = function() {
     $scope.restaurants.push({ name: $scope.restaurantName,
-      type: $scope.restaurantType,
+      type: [$scope.restaurantType],
       price: $scope.restaurantPrice });
 
       $scope.restaurantName = null;
